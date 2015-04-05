846f480e4110f3d7f4cd5259d8226dfdd288b147
Markos Fragkakis <markos.fragkakis@gmail.com>
2015-04-05 23:49:16 +0300

    Adding projects client

---
+++ src/main/java/org/fragkakis/txclient/model/Project.java
@@ -0,0 +1,57 @@
+package org.fragkakis.txclient.model;
+
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Project {
+
+    private String slug;
+    private String name;
+    private String description;
+    private String source_language_code;
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSource_language_code() {
+        return source_language_code;
+    }
+
+    public void setSource_language_code(String source_language_code) {
+        this.source_language_code = source_language_code;
+    }
+}
